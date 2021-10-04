aa0e73fc6ef7df3de5f72abb4db9da2238896f12
Nastya <nastya.kuzmina200@mail.ru>
2021-10-04 23:08:42 +0300

    initcommit

---
+++ App/Program.cs
@@ -0,0 +1,19 @@
+﻿using Models;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace App
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please write a full path of a directory:");
+            string path = Console.ReadLine();
+
+            RecognitionModel model = new RecognitionModel(path);
+            model.Recognize();
+        }
+    }
+}
---
+++ RecognitionModel/RecognitionResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class RecognitionResponse
+    {
+        public Dictionary<string, int> FoundObjects { get; set; }
+        public int Percent { get; set; }
+        public List<List<float>> Corners { get; set; }
+        public RecognitionResponse()
+        {
+            FoundObjects = new Dictionary<string, int>();
+            Corners = new List<List<float>>();
+        }
+    }
+}

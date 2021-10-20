eff087d66978a6cb640ae55361b8bf0e00fe5b3d
kuzminastya <57797237+kuzminastya@users.noreply.github.com>
2021-10-20 19:53:34 +0300

    initcommit (#1)
    Первое задание

---
+++ App/Program.cs
@@ -0,0 +1,42 @@
+﻿using Models;
+using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace App
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please write a full path of a directory:");
+            string path = Console.ReadLine();
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            RecognitionModel model = new RecognitionModel(path, cts);
+            ConcurrentQueue<RecognitionResponse> responseQueue = new ConcurrentQueue<RecognitionResponse>();
+
+            var t = Task.Run(() =>
+            {
+                if (!cts.Token.IsCancellationRequested)
+                {
+                    for (int i = 0; i < Directory.GetFiles(path).Length; i++)
+                    {
+                        while (responseQueue.Count == 0) { }
+                        responseQueue.TryDequeue(out var res);
+                        Console.Write($"\n{res.Percent}% images are processed. ");
+                        foreach (var obj in res.FoundObjects)
+                            Console.Write($"{obj.Key}: {obj.Value}, ");
+                        Console.WriteLine();
+                    }
+                }
+            }, cts.Token);
+
+            model.Recognize(responseQueue);
+
+            t.Wait();
+        }
+    }
+}
---
+++ RecognitionModel/YoloV4BitmapData.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.ML.Data;
+using Microsoft.ML.Transforms.Image;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Models
+{
+    public class YoloV4BitmapData
+    {
+        [ColumnName("bitmap")]
+        [ImageType(416, 416)]
+        public Bitmap Image { get; set; }
+
+        [ColumnName("width")]
+        public float ImageWidth => Image.Width;
+
+        [ColumnName("height")]
+        public float ImageHeight => Image.Height;
+    }
+}
